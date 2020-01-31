e706c1fd6f8c714577ba690f5d72e90bcb956da4
jwells89 <john.ivan@me.com>
2020-01-30 20:57:14 -0800

    Initial Commit

---
+++ Seeker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Seeker
+//
+//  Created by John Wells on 1/30/20.
+//  Copyright © 2020 John Wells. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
